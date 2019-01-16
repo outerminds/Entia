a2a951713ce72085cfa45b645b0fb154fa5c40e6
Magicolo <straty91@hotmail.com>
2019-01-15 20:34:34 -0500

    - fixed: system injectors would inject in static fields

--- Entia/Control/Injectors.cs
+++ Entia/Control/Injectors.cs
@@ -7,23 +7,23 @@ using System.Reflection;
 
 namespace Entia.Injectors
 {
-	public sealed class System : Injector<ISystem>
-	{
-		public override Result<ISystem> Inject(MemberInfo member, World world)
-		{
-			switch (member)
-			{
-				case Type type:
-					return Result
-						.Try(Activator.CreateInstance, type)
-						.Cast<ISystem>()
-						.Bind(instance => type.GetFields()
-							.Select(field => world.Injectors().Inject(field).Do((object current) => field.SetValue(instance, current)))
-							.All()
-							.Return(instance));
-				case FieldInfo field: return Inject(field.FieldType, world);
-				default: return Failure.Empty;
-			}
-		}
-	}
+    public sealed class System : Injector<ISystem>
+    {
+        public override Result<ISystem> Inject(MemberInfo member, World world)
+        {
+            switch (member)
+            {
+                case Type type:
+                    return Result
+                        .Try(Activator.CreateInstance, type)
+                        .Cast<ISystem>()
+                        .Bind(instance => TypeUtility.GetFields(type, TypeUtility.PublicInstance)
+                            .Select(field => world.Injectors().Inject(field).Do((object current) => field.SetValue(instance, current)))
+                            .All()
+                            .Return(instance));
+                case FieldInfo field: return Inject(field.FieldType, world);
+                default: return Failure.Empty;
+            }
+        }
+    }
 }
