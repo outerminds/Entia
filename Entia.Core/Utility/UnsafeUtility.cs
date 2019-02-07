98096efa148d9adae90d70c0eaf363b4e277ea4f
Magicolo <straty91@hotmail.com>
2019-02-06 23:25:42 -0500

    - additionnal protection in UnsafeUtility

--- Entia.Core/Utility/UnsafeUtility.cs
+++ Entia.Core/Utility/UnsafeUtility.cs
@@ -20,19 +20,34 @@ namespace Entia.Core
             {
                 if (typeof(T).IsValueType)
                 {
-                    var array = new T[2];
-                    var head = Cast<T>.ToPointer(ref array[0]);
-                    var tail = Cast<T>.ToPointer(ref array[1]);
-                    var size = tail.ToInt64() - head.ToInt64();
-                    Value = (int)size;
+                    // NOTE: since the array cannot be pinned because type 'T' cannot be ensured to be blittable, precautions are taken for the unlikely
+                    // case where the garbage collector moves the array between 2 pointer get.
+                    while (true)
+                    {
+                        var array = new T[3];
+                        var a = Cast<T>.ToPointer(ref array[0]).ToInt64();
+                        var b = Cast<T>.ToPointer(ref array[1]).ToInt64();
+                        var c = Cast<T>.ToPointer(ref array[2]).ToInt64();
+                        if (a > b || a > c || b > c) continue;
+
+                        var sizeAB = b - a;
+                        var sizeBC = c - b;
+                        var sizeAC = c - a;
+                        if (sizeAB == sizeBC && sizeAC == sizeAB + sizeBC)
+                        {
+                            Value = (int)sizeAB;
+                            break;
+                        }
+                    }
                 }
             }
         }
 
         public static class Cast<T>
         {
-            public static readonly ReferenceReturn<T> ToReference = _ => throw null;
-            public static readonly PointerReturn<T> ToPointer = (ref T _) => throw null;
+            // NOTE: these function pointers are replaced by the one in '_return'.
+            public static readonly ReferenceReturn<T> ToReference = _ => throw new NotImplementedException();
+            public static readonly PointerReturn<T> ToPointer = (ref T _) => throw new NotImplementedException();
 
             static Cast()
             {
@@ -47,7 +62,8 @@ namespace Entia.Core
 
         public static class Cast<TSource, TTarget>
         {
-            public static readonly Return<TSource, TTarget> To = (ref TSource _) => throw null;
+            // NOTE: this function pointer is replaced by the one in '_return'.
+            public static readonly Return<TSource, TTarget> To = (ref TSource _) => throw new NotImplementedException();
 
             static Cast()
             {
