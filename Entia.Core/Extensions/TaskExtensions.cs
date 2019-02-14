a3feb7dad9a8da7b782d526449f0bc30469cec0b
Magicolo <straty91@hotmail.com>
2019-02-14 17:52:05 -0500

    - notes - task extensions

--- Entia.Core/Extensions/TaskExtensions.cs
+++ Entia.Core/Extensions/TaskExtensions.cs
@@ -7,18 +7,50 @@ namespace Entia.Core
 {
     public static class TaskExtensions
     {
-        public static Task Timeout(this Task task, TimeSpan timeout) => Task.Run(async () =>
+        public static Task Timeout(this Task task, TimeSpan timeout, CancellationTokenSource cancel = null) => Task.Run(async () =>
         {
             var timer = Stopwatch.StartNew();
-            while (task.Status == TaskStatus.Running) if (timer.Elapsed >= timeout) throw new TimeoutException();
+            while (task.Status == TaskStatus.Running)
+            {
+                if (timer.Elapsed >= timeout)
+                {
+                    cancel?.Cancel();
+                    throw new TimeoutException();
+                }
+            }
             await task;
         });
 
-        public static Task<T> Timeout<T>(this Task<T> task, TimeSpan timeout) => Task.Run(async () =>
+        public static Task<T> Timeout<T>(this Task<T> task, TimeSpan timeout, CancellationTokenSource cancel = null) => Task.Run(async () =>
         {
             var timer = Stopwatch.StartNew();
-            while (task.Status == TaskStatus.Running) if (timer.Elapsed >= timeout) throw new TimeoutException();
+            while (task.Status == TaskStatus.Running)
+            {
+                if (timer.Elapsed >= timeout)
+                {
+                    cancel?.Cancel();
+                    throw new TimeoutException();
+                }
+            }
             return await task;
+        });
+
+        public static Task Except<T>(this Task task, Action<T> handle) where T : Exception => Task.Run(async () =>
+        {
+            try { await task; }
+            catch (T exception) { handle(exception); }
+        });
+
+        public static Task Except(this Task task, Action<Exception> handle) => Task.Run(async () =>
+        {
+            try { await task; }
+            catch (Exception exception) { handle(exception); }
+        });
+
+        public static Task Do(this Task task, Action @do) => Task.Run(async () =>
+        {
+            try { await task; @do(); }
+            catch { throw; }
         });
     }
 }
