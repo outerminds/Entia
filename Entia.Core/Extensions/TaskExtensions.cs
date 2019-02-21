7d51fe6c32a2bb0d7d2441dc069a9cb6ef02c8f7
Magicolo <straty91@hotmail.com>
2019-02-21 13:32:26 -0500

    - fixed TaskExtensions

--- Entia.Core/Extensions/TaskExtensions.cs
+++ Entia.Core/Extensions/TaskExtensions.cs
@@ -7,32 +7,10 @@ namespace Entia.Core
 {
     public static class TaskExtensions
     {
-        public static Task Timeout(this Task task, TimeSpan timeout, CancellationTokenSource cancel = null) => Task.Run(async () =>
+        public static Task Timeout(this Task task, TimeSpan timeout) => Task.Run(async () =>
         {
-            var timer = Stopwatch.StartNew();
-            while (task.Status == TaskStatus.Running)
-            {
-                if (timer.Elapsed >= timeout)
-                {
-                    cancel?.Cancel();
-                    throw new TimeoutException();
-                }
-            }
-            await task;
-        });
-
-        public static Task<T> Timeout<T>(this Task<T> task, TimeSpan timeout, CancellationTokenSource cancel = null) => Task.Run(async () =>
-        {
-            var timer = Stopwatch.StartNew();
-            while (task.Status == TaskStatus.Running)
-            {
-                if (timer.Elapsed >= timeout)
-                {
-                    cancel?.Cancel();
-                    throw new TimeoutException();
-                }
-            }
-            return await task;
+            if (task == await Task.WhenAny(task, Task.Delay(timeout))) await task;
+            else throw new TimeoutException();
         });
 
         public static Task Except<T>(this Task task, Action<T> handle) where T : Exception => Task.Run(async () =>
@@ -49,8 +27,8 @@ namespace Entia.Core
 
         public static Task Do(this Task task, Action @do) => Task.Run(async () =>
         {
-            try { await task; @do(); }
-            catch { throw; }
+            await task;
+            @do();
         });
     }
 }
