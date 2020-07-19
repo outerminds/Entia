cb845e6c582cb49a7e0a515d6b5b077bb9b22929
Magicolo <magicololand@gmail.com>
2020-07-18 22:01:08 -0400

    - minor refactor

--- Entia.Core/Or.cs
+++ Entia.Core/Or.cs
@@ -38,39 +38,39 @@ namespace Entia.Core
         public override int GetHashCode() => EqualityComparer<T>.Default.GetHashCode(Value);
     }
 
-    public readonly struct Or<T1, T2> : IOr, IEquatable<Or<T1, T2>>, IEquatable<Left<T1>>, IEquatable<Right<T2>>
+    public readonly struct Or<TLeft, TRight> : IOr, IEquatable<Or<TLeft, TRight>>, IEquatable<Left<TLeft>>, IEquatable<Right<TRight>>
     {
-        public static bool operator ==(in Or<T1, T2> or, in T1 left) =>
-            or.TryLeft(out var value) && EqualityComparer<T1>.Default.Equals(value, left);
-        public static bool operator ==(in Or<T1, T2> or, in T2 right) =>
-            or.TryRight(out var value) && EqualityComparer<T2>.Default.Equals(value, right);
-        public static bool operator ==(in Or<T1, T2> left, in Or<T1, T2> right) =>
+        public static bool operator ==(in Or<TLeft, TRight> or, in TLeft left) =>
+            or.TryLeft(out var value) && EqualityComparer<TLeft>.Default.Equals(value, left);
+        public static bool operator ==(in Or<TLeft, TRight> or, in TRight right) =>
+            or.TryRight(out var value) && EqualityComparer<TRight>.Default.Equals(value, right);
+        public static bool operator ==(in Or<TLeft, TRight> left, in Or<TLeft, TRight> right) =>
             (left.TryLeft(out var leftValue) && right == leftValue) ||
             (left.TryRight(out var rightValue) && right == rightValue);
-        public static bool operator ==(in T1 left, in Or<T1, T2> or) => or == left;
-        public static bool operator ==(in T2 right, in Or<T1, T2> or) => or == right;
-        public static bool operator !=(in Or<T1, T2> or, in T1 left) => !(or == left);
-        public static bool operator !=(in Or<T1, T2> or, in T2 right) => !(or == right);
-        public static bool operator !=(in Or<T1, T2> left, in Or<T1, T2> right) => !(left == right);
-        public static bool operator !=(in T1 left, in Or<T1, T2> or) => !(left == or);
-        public static bool operator !=(in T2 right, in Or<T1, T2> or) => !(right == or);
-        public static implicit operator Or<T1, T2>(in Left<T1> left) => left.Value;
-        public static implicit operator Or<T1, T2>(in Right<T2> right) => right.Value;
-        public static implicit operator Or<T1, T2>(in T1 value) => new Or<T1, T2>(Or.Tags.Left, value, default);
-        public static implicit operator Or<T1, T2>(in T2 value) => new Or<T1, T2>(Or.Tags.Right, default, value);
-        public static explicit operator T1(in Or<T1, T2> or) => or.IsLeft() ? or._left : throw new InvalidCastException();
-        public static explicit operator T2(in Or<T1, T2> or) => or.IsRight() ? or._right : throw new InvalidCastException();
-        public static explicit operator Left<T1>(in Or<T1, T2> or) => (T1)or;
-        public static explicit operator Right<T2>(in Or<T1, T2> or) => (T2)or;
+        public static bool operator ==(in TLeft left, in Or<TLeft, TRight> or) => or == left;
+        public static bool operator ==(in TRight right, in Or<TLeft, TRight> or) => or == right;
+        public static bool operator !=(in Or<TLeft, TRight> or, in TLeft left) => !(or == left);
+        public static bool operator !=(in Or<TLeft, TRight> or, in TRight right) => !(or == right);
+        public static bool operator !=(in Or<TLeft, TRight> left, in Or<TLeft, TRight> right) => !(left == right);
+        public static bool operator !=(in TLeft left, in Or<TLeft, TRight> or) => !(left == or);
+        public static bool operator !=(in TRight right, in Or<TLeft, TRight> or) => !(right == or);
+        public static implicit operator Or<TLeft, TRight>(in Left<TLeft> left) => left.Value;
+        public static implicit operator Or<TLeft, TRight>(in Right<TRight> right) => right.Value;
+        public static implicit operator Or<TLeft, TRight>(in TLeft value) => new Or<TLeft, TRight>(Or.Tags.Left, value, default);
+        public static implicit operator Or<TLeft, TRight>(in TRight value) => new Or<TLeft, TRight>(Or.Tags.Right, default, value);
+        public static explicit operator TLeft(in Or<TLeft, TRight> or) => or.IsLeft() ? or._left : throw new InvalidCastException();
+        public static explicit operator TRight(in Or<TLeft, TRight> or) => or.IsRight() ? or._right : throw new InvalidCastException();
+        public static explicit operator Left<TLeft>(in Or<TLeft, TRight> or) => (TLeft)or;
+        public static explicit operator Right<TRight>(in Or<TLeft, TRight> or) => (TRight)or;
 
         public Or.Tags Tag { get; }
-        public Option<T1> Left => TryLeft(out var value) ? Option.From(value) : Option.None();
-        public Option<T2> Right => TryRight(out var value) ? Option.From(value) : Option.None();
-        readonly T1 _left;
-        readonly T2 _right;
+        public Option<TLeft> Left => TryLeft(out var value) ? Option.From(value) : Option.None();
+        public Option<TRight> Right => TryRight(out var value) ? Option.From(value) : Option.None();
+        readonly TLeft _left;
+        readonly TRight _right;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        Or(Or.Tags tag, in T1 left, in T2 right)
+        Or(Or.Tags tag, in TLeft left, in TRight right)
         {
             Tag = tag;
             _left = left;
@@ -78,14 +78,14 @@ namespace Entia.Core
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public bool TryLeft(out T1 value)
+        public bool TryLeft(out TLeft value)
         {
             value = _left;
             return Tag == Or.Tags.Left;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public bool TryRight(out T2 value)
+        public bool TryRight(out TRight value)
         {
             value = _right;
             return Tag == Or.Tags.Right;
@@ -105,14 +105,14 @@ namespace Entia.Core
             _ => throw new InvalidOperationException()
         };
 
-        public bool Equals(Or<T1, T2> other) => this == other;
-        public bool Equals(Left<T1> other) => this == other;
-        public bool Equals(Right<T2> other) => this == other;
+        public bool Equals(Or<TLeft, TRight> other) => this == other;
+        public bool Equals(Left<TLeft> other) => this == other;
+        public bool Equals(Right<TRight> other) => this == other;
         public override bool Equals(object obj) =>
-            obj is T1 leftValue ? this == leftValue :
-            obj is T2 rightValue ? this == rightValue :
-            obj is Left<T1> left ? this == left :
-            obj is Right<T2> right && this == right;
+            obj is TLeft leftValue ? this == leftValue :
+            obj is TRight rightValue ? this == rightValue :
+            obj is Left<TLeft> left ? this == left :
+            obj is Right<TRight> right && this == right;
     }
 
     public static class Or
@@ -120,19 +120,19 @@ namespace Entia.Core
         public enum Tags { None, Left, Right }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool Is<T1, T2>(in this Or<T1, T2> or, Tags tag) => or.Tag == tag;
+        public static bool Is<TLeft, TRight>(in this Or<TLeft, TRight> or, Tags tag) => or.Tag == tag;
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsLeft<T1, T2>(in this Or<T1, T2> or) => or.Is(Tags.Left);
+        public static bool IsLeft<TLeft, TRight>(in this Or<TLeft, TRight> or) => or.Is(Tags.Left);
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsRight<T1, T2>(in this Or<T1, T2> or) => or.Is(Tags.Right);
+        public static bool IsRight<TLeft, TRight>(in this Or<TLeft, TRight> or) => or.Is(Tags.Right);
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Left<T> Left<T>(in T value) => value;
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Right<T> Right<T>(in T value) => value;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Or<T2, T1> Flip<T1, T2>(in this Or<T1, T2> or) =>
-            or.Match(left => Right(left).AsOr<T2>(), right => Left(right));
+        public static Or<TRight, TLeft> Flip<TLeft, TRight>(in this Or<TLeft, TRight> or) =>
+            or.Match(left => Right(left).AsOr<TRight>(), right => Left(right));
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Or<T, Unit> AsOr<T>(in this Option<T> option) =>
             option.Match(value => Left(value).AsOr<Unit>(), () => Right(default(Unit)));
@@ -147,55 +147,55 @@ namespace Entia.Core
             or.Match(value => Result.Success(value).AsResult(), messages => Result.Failure(messages));
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static TOut Match<T1, T2, TOut>(in this Or<T1, T2> or, Func<T1, TOut> left, Func<T2, TOut> right) =>
+        public static TOut Match<TLeft, TRight, TOut>(in this Or<TLeft, TRight> or, Func<TLeft, TOut> left, Func<TRight, TOut> right) =>
             or.TryLeft(out var leftValue) ? left(leftValue) :
             or.TryRight(out var rightValue) ? right(rightValue) :
             throw new InvalidOperationException();
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static TOut Match<T1, T2, TState, TOut>(in this Or<T1, T2> or, in TState state, Func<T1, TState, TOut> left, Func<T2, TState, TOut> right) =>
+        public static TOut Match<TLeft, TRight, TState, TOut>(in this Or<TLeft, TRight> or, in TState state, Func<TLeft, TState, TOut> left, Func<TRight, TState, TOut> right) =>
             or.TryLeft(out var leftValue) ? left(leftValue, state) :
             or.TryRight(out var rightValue) ? right(rightValue, state) :
             throw new InvalidOperationException();
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Or<TTarget, T> MapLeft<TSource, TTarget, T, TState>(in this Or<TSource, T> or, in TState state, Func<TSource, TState, TTarget> map) =>
-            or.Match((map, state), (value, state) => Left(state.map(value, state.state)).AsOr<T>(), (value, _) => Right(value));
+        public static Or<TTarget, TRight> MapLeft<TSource, TTarget, TRight, TState>(in this Or<TSource, TRight> or, in TState state, Func<TSource, TState, TTarget> map) =>
+            or.Match((map, state), (value, state) => Left(state.map(value, state.state)).AsOr<TRight>(), (value, _) => Right(value));
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Or<TTarget, T> MapLeft<TSource, TTarget, T>(in this Or<TSource, T> or, Func<TSource, TTarget> map) =>
-            or.Match(map, (value, state) => Left(state(value)).AsOr<T>(), (value, _) => Right(value));
+        public static Or<TTarget, TRight> MapLeft<TSource, TTarget, TRight>(in this Or<TSource, TRight> or, Func<TSource, TTarget> map) =>
+            or.Match(map, (value, state) => Left(state(value)).AsOr<TRight>(), (value, _) => Right(value));
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Or<T, TTarget> MapRight<T, TSource, TTarget, TState>(in this Or<T, TSource> or, in TState state, Func<TSource, TState, TTarget> map) =>
+        public static Or<TLeft, TTarget> MapRight<TLeft, TSource, TTarget, TState>(in this Or<TLeft, TSource> or, in TState state, Func<TSource, TState, TTarget> map) =>
             or.Match((map, state), (value, _) => Left(value).AsOr<TTarget>(), (value, state) => Right(state.map(value, state.state)));
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Or<T, TTarget> MapRight<T, TSource, TTarget>(in this Or<T, TSource> or, Func<TSource, TTarget> map) =>
+        public static Or<TLeft, TTarget> MapRight<TLeft, TSource, TTarget>(in this Or<TLeft, TSource> or, Func<TSource, TTarget> map) =>
             or.Match(map, (value, _) => Left(value).AsOr<TTarget>(), (value, state) => Right(state(value)));
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Or<T1, T2> DoLeft<T1, T2, TState>(in this Or<T1, T2> or, in TState state, Action<T1, TState> @do) =>
+        public static Or<TLeft, TRight> DoLeft<TLeft, TRight, TState>(in this Or<TLeft, TRight> or, in TState state, Action<TLeft, TState> @do) =>
             or.MapLeft((@do, state), (value, state) => { state.@do(value, state.state); return value; });
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Or<T1, T2> DoLeft<T1, T2>(in this Or<T1, T2> or, Action<T1> @do) =>
+        public static Or<TLeft, TRight> DoLeft<TLeft, TRight>(in this Or<TLeft, TRight> or, Action<TLeft> @do) =>
             or.MapLeft(@do, (value, state) => { state(value); return value; });
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Or<T1, T2> DoRight<T1, T2, TState>(in this Or<T1, T2> or, in TState state, Action<T2, TState> @do) =>
+        public static Or<TLeft, TRight> DoRight<TLeft, TRight, TState>(in this Or<TLeft, TRight> or, in TState state, Action<TRight, TState> @do) =>
             or.MapRight((@do, state), (value, state) => { state.@do(value, state.state); return value; });
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Or<T1, T2> DoRight<T1, T2>(in this Or<T1, T2> or, Action<T2> @do) =>
+        public static Or<TLeft, TRight> DoRight<TLeft, TRight>(in this Or<TLeft, TRight> or, Action<TRight> @do) =>
             or.MapRight(@do, (value, state) => { state(value); return value; });
 
-        public static IEnumerable<T1> Lefts<T1, T2>(this IEnumerable<Or<T1, T2>> ors)
+        public static IEnumerable<TLeft> Lefts<TLeft, TRight>(this IEnumerable<Or<TLeft, TRight>> ors)
         {
             foreach (var or in ors) if (or.TryLeft(out var value)) yield return value;
         }
 
-        public static IEnumerable<T2> Rights<T1, T2>(this IEnumerable<Or<T1, T2>> ors)
+        public static IEnumerable<TRight> Rights<TLeft, TRight>(this IEnumerable<Or<TLeft, TRight>> ors)
         {
             foreach (var or in ors) if (or.TryRight(out var value)) yield return value;
         }
