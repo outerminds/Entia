a2301df9a60f07cfccc4d1a08c6c274d7d1d5329
Magicolo <magicololand@gmail.com>
2020-10-23 15:41:28 -0400

    - minor refactor

--- Entia.Core/Collections/TypeMap.cs
+++ Entia.Core/Collections/TypeMap.cs
@@ -181,22 +181,27 @@ namespace Entia.Core
         static Entry GetEntry(Type type)
         {
             if (_typeToEntry.TryGetValue(type, out var entry)) return entry;
-            lock (_typeToEntry)
+            return CreateEntry(type);
+
+            static Entry CreateEntry(Type type)
             {
-                if (_typeToEntry.TryGetValue(type, out entry)) return entry;
-                // 'super', 'sub' and 'entry' must stay within the lock since they access '_entries' which can be
-                // modified by other threads and could otherwise lead to race conditions.
-                var super = _entries.Where(type, (current, state) => state.Is(current.Type, true, true)).ToArray();
-                var sub = _entries.Where(type, (current, state) => current.Type.Is(state, true, true)).ToArray();
-                entry = new Entry(type, _entries.Length, super, sub);
-                foreach (var current in super) Interlocked.Exchange(ref current.Sub, current.Sub.Append(entry));
-                foreach (var current in sub) Interlocked.Exchange(ref current.Super, current.Super.Append(entry));
-                // Technically, a thread may observe an entry that does not yet exist in the entry dictionary by
-                // trying to guess the index of that entry, but such hackish speculation will not be considered.
-                Interlocked.Exchange(ref _entries, _entries.Append(entry));
-                // This must happen last to make sure that no thread can observe an entry that is not full initialized.
-                Interlocked.Exchange(ref _typeToEntry, new Dictionary<Type, Entry>(_typeToEntry) { { type, entry } });
-                return entry;
+                lock (_typeToEntry)
+                {
+                    if (_typeToEntry.TryGetValue(type, out var entry)) return entry;
+                    // 'super', 'sub' and 'entry' must stay within the lock since they access '_entries' which can be
+                    // modified by other threads and could otherwise lead to race conditions.
+                    var super = _entries.Where(type, (current, state) => state.Is(current.Type, true, true)).ToArray();
+                    var sub = _entries.Where(type, (current, state) => current.Type.Is(state, true, true)).ToArray();
+                    entry = new Entry(type, _entries.Length, super, sub);
+                    foreach (var current in super) Interlocked.Exchange(ref current.Sub, current.Sub.Append(entry));
+                    foreach (var current in sub) Interlocked.Exchange(ref current.Super, current.Super.Append(entry));
+                    // Technically, a thread may observe an entry that does not yet exist in the entry dictionary by
+                    // trying to guess the index of that entry, but such hackish speculation will not be considered.
+                    Interlocked.Exchange(ref _entries, _entries.Append(entry));
+                    // This must happen last to make sure that no thread can observe an entry that is not full initialized.
+                    Interlocked.Exchange(ref _typeToEntry, new Dictionary<Type, Entry>(_typeToEntry) { { type, entry } });
+                    return entry;
+                }
             }
         }
 
